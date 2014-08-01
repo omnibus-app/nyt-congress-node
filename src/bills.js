2b3185ba1e070c951e1805ebcab924a57ceba829
Nick Bottomley <nhbottomley@gmail.com>
2014-08-01 11:27:01 -0700

    fix 'type' params across endpoints

--- src/bills.js
+++ src/bills.js
@@ -1,6 +1,6 @@
 module.exports = {
-    billsRecent: "http://api.nytimes.com/svc/politics/{version}/us/legislative/congress/{congress-number}/{chamber}/bills/{type}{response-format}",
-    billsByMember: "http://api.nytimes.com/svc/politics/{version}/us/legislative/congress/members/{member-id}/bills/{type}{response-format}",
+    billsRecent: "http://api.nytimes.com/svc/politics/{version}/us/legislative/congress/{congress-number}/{chamber}/bills/{bill-type}{response-format}",
+    billsByMember: "http://api.nytimes.com/svc/politics/{version}/us/legislative/congress/members/{member-id}/bills/{bill-type}{response-format}",
     billDetails: "http://api.nytimes.com/svc/politics/{version}/us/legislative/congress/{congress-number}/bills/{bill-id}{response-format}",
     billSubjects: "http://api.nytimes.com/svc/politics/{version}/us/legislative/congress/{congress-number}/bills/{bill-id}/subjects{response-format}",
     billAmendments: "http://api.nytimes.com/svc/politics/{version}/us/legislative/congress/{congress-number}/bills/{bill-id}/amendments{response-format}",
